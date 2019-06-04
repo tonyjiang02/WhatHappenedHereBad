104116dbb5372c7a0776bd70db2665634e74298b
bruisedllama <tonyjiang02@gmail.com>
2019-06-04 16:33:29 -0700

    stuff

--- DustBowlScene.gd
+++ DustBowlScene.gd
@@ -7,24 +7,40 @@ signal talk_muley
 signal enter_house
 var nextToHouse = false
 var nextToMuley = false
-# Called when the node enters the scene tree for the first time.
+var talkingToMuley = false
+var file = File.new()
+var dict = {}
+var dialogue_counter = 1
+var ignoreinputs = false
 func _ready():
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	if global.pos1:
+		$Player.position = global.pos1
+	$DialogueBox.hide()
+	file.open("res://textfiles/dialogueMuley.json", file.READ)
+	var text = file.get_as_text()
+	var result = JSON.parse(text)
+	dict = result.result
+	file.close()
 func _process(delta):
 	_get_Message()
-	
+	global.pos1 = $Player.position
 func _input(e):
-	if Input.is_key_pressed(KEY_F):
+	if !ignoreinputs:
+		$Timer.start()
+	if Input.is_key_pressed(KEY_F) and !ignoreinputs:
 		if nextToHouse:
 			emit_signal("enter_house")
 			print("enter house")
+			get_tree().change_scene("res://InsideMuleyGravesHouse.tscn")
 		if nextToMuley:
 			emit_signal("talk_muley")
 			print("talk_muley")
+			_display_next_dialogue()
+			talkingToMuley = true
+	if Input.is_key_pressed(KEY_C) and !ignoreinputs:
+		if talkingToMuley:
+			_display_next_dialogue()
+	ignoreinputs = true
 func _get_Message():
 	var distanceToMuley = $MuleyGraves.position.distance_to($Player.position)
 	var distanceToHouse = $DustBowlHouse.position.distance_to($Player.position)
@@ -36,4 +52,21 @@ func _get_Message():
 		$Label.text = "PRESS F TO TALK TO MULEY"
 		nextToHouse = false
 		nextToMuley = true
-	
+
+func _display_next_dialogue():
+	if dict.has(str(dialogue_counter)):
+		$DialogueBox.clear()
+		$DialogueBox.show()
+		var display_text = dict[str(dialogue_counter)].content
+		var speaker = dict[str(dialogue_counter)].name
+		$DialogueBox.add_text(speaker + " : " + display_text)
+		dialogue_counter+=1
+	else:
+		dialogue_counter = 1
+		$DialogueBox.hide()
+		$DialogueBox.clear()
+		talkingToMuley = false
+		
+
+func _on_Timer_timeout():
+	ignoreinputs = false
